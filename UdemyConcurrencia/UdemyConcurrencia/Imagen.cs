d9cbf15ecf86e3205a3cd5931c4565b480e890cb
cesar <cesarluilly.garcia@hotmail.com>
2021-04-26 21:50:33 -0500

    Video 51 - Con TaskWhenAll ejecutamos tareas simultaneas

---
+++ UdemyConcurrencia/UdemyConcurrencia/Imagen.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdemyConcurrencia
+{
+    class Imagen
+    {
+        public String Nombre { get; set; }
+        public String URL { get; set; }
+
+    }
+}
